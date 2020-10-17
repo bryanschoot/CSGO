d3138a37a5f82777401cfe48ddfb35b285b96ef8
Bryan Schoot <bdjschoot@gmail.com>
2020-10-17 16:48:01 +0200

    Sys added for extanal windows api

---
+++ CSGO.Sys/User32.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CSGO.Sys
+{
+    public static class User32
+    {
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-clienttoscreen
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ClientToScreen(IntPtr hWnd, out Win32.Point lpPoint);
+
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getclientrect
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool GetClientRect(IntPtr hWnd, out Win32.Rect lpRect);
+
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getforegroundwindow
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetForegroundWindow();
+
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-findwindowa
+        /// </summary>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+    }
+}
---
+++ CSGO.Sys/Win32.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSGO.Sys
+{
+    public static class Win32
+    {
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/windef/ns-windef-rect
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int Left, Top, Right, Bottom;
+        }
+
+        /// <summary>
+        ///     https://docs.microsoft.com/en-us/windows/win32/api/windef/ns-windef-point
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Point
+        {
+            public int X, Y;
+        }
+    }
+}
