b3ae0176e9f82b4643ba307041de04fca5a0f843
Bryan Schoot <bdjschoot@gmail.com>
2020-10-17 16:45:44 +0200

    Console added for visuals

---
+++ CSGO.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CSGO.Data;
+
+namespace CSGO.Console
+{
+    public class Program
+    {
+        /// <inheritdoc cref="Game" />
+        private Game Game { get; set; }
+
+        private static void Main()
+        {
+            var game = new Game();
+            game.Start();
+
+            do
+            {
+                while (!System.Console.KeyAvailable)
+                {
+                    // Do something
+                }
+            } while (System.Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            game.Dispose();
+        }
+    }
+}
