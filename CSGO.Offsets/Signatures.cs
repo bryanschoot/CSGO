08205e669a221a3c6894dc10eec349281f8e7b43
Bryan Schoot <bdjschoot@gmail.com>
2020-10-17 19:00:44 +0200

    Added Offsets of the game from Hazedumper

---
+++ CSGO.Offsets/Signatures.cs
@@ -0,0 +1,63 @@
+﻿namespace CSGO.Offsets
+{
+    public class Signatures
+    {
+        public const int anim_overlays = 0x2980;
+        public const int clientstate_choked_commands = 0x4D30;
+        public const int clientstate_delta_ticks = 0x174;
+        public const int clientstate_last_outgoing_command = 0x4D2C;
+        public const int clientstate_net_channel = 0x9C;
+        public const int convar_name_hash_table = 0x2F0F8;
+        public const int dwClientState = 0x58CFE4;
+        public const int dwClientState_GetLocalPlayer = 0x180;
+        public const int dwClientState_IsHLTV = 0x4D48;
+        public const int dwClientState_Map = 0x28C;
+        public const int dwClientState_MapDirectory = 0x188;
+        public const int dwClientState_MaxPlayer = 0x388;
+        public const int dwClientState_PlayerInfo = 0x52C0;
+        public const int dwClientState_State = 0x108;
+        public const int dwClientState_ViewAngles = 0x4D90;
+        public const int dwEntityList = 0x4D523AC;
+        public const int dwForceAttack = 0x3183930;
+        public const int dwForceAttack2 = 0x318393C;
+        public const int dwForceBackward = 0x31838E8;
+        public const int dwForceForward = 0x3183954;
+        public const int dwForceJump = 0x51FBFB8;
+        public const int dwForceLeft = 0x31838DC;
+        public const int dwForceRight = 0x3183900;
+        public const int dwGameDir = 0x62B7F8;
+        public const int dwGameRulesProxy = 0x526F2B4;
+        public const int dwGetAllClasses = 0xD643A4;
+        public const int dwGlobalVars = 0x58CCE8;
+        public const int dwGlowObjectManager = 0x529A1E0;
+        public const int dwInput = 0x51A3978;
+        public const int dwInterfaceLinkList = 0x9054E4;
+        public const int dwLocalPlayer = 0xD3DD14;
+        public const int dwMouseEnable = 0xD438B8;
+        public const int dwMouseEnablePtr = 0xD43888;
+        public const int dwPlayerResource = 0x3181C80;
+        public const int dwRadarBase = 0x518710C;
+        public const int dwSensitivity = 0xD43754;
+        public const int dwSensitivityPtr = 0xD43728;
+        public const int dwSetClanTag = 0x8A070;
+        public const int dwViewMatrix = 0x4D43CC4;
+        public const int dwWeaponTable = 0x51A443C;
+        public const int dwWeaponTableIndex = 0x325C;
+        public const int dwYawPtr = 0xD43518;
+        public const int dwZoomSensitivityRatioPtr = 0xD487B8;
+        public const int dwbSendPackets = 0xD603A;
+        public const int dwppDirect3DDevice9 = 0xA7050;
+        public const int find_hud_element = 0x2D4A1BE0;
+        public const int force_update_spectator_glow = 0x3A0E02;
+        public const int interface_engine_cvar = 0x3E9EC;
+        public const int is_c4_owner = 0x3AD890;
+        public const int m_bDormant = 0xED;
+        public const int m_flSpawnTime = 0xA370;
+        public const int m_pStudioHdr = 0x294C;
+        public const int m_pitchClassPtr = 0x51873B0;
+        public const int m_yawClassPtr = 0xD43518;
+        public const int model_ambient_min = 0x59005C;
+        public const int set_abs_angles = 0x1D4400;
+        public const int set_abs_origin = 0x1D4240;
+    }
+}
